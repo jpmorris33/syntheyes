270ee08f85eac9e175f4ae4fb1e063c135661f3c
jpmorris <jpmorris@Pettersohn.(none)>
2024-08-21 21:15:15 +0100

    feat(unicorn) - example code now drives two Unicorn panels from one Arduino

--- unicorn/syntheyes-unicorn.ino
+++ unicorn/syntheyes-unicorn.ino
@@ -1,12 +1,6 @@
 //
-//  Synth Eyes for Arduino (Unicorn Hat HD version)
-//  V3.1.4 - Add hook for internal LCD screen messages, change checkExpression API
-//  V3.1.3 - Happy animation, fault animation with red status lights, fix status lights blip
-//  V3.1.2 - Blushing animation
-//  V3.1.1 - ACK LED and voice detection for status lights
-//  V3.1.0 - Optionally drive neopixel status lights as well
-//  V3.0.1 - Bug fix
-//  V3.0.0 - Restructure to use procedural blinking
+//  Synth Eyes for Arduino
+//  Unicorn Hat HD example
 //
 //  Based on example code from  https://gist.github.com/nrdobie/8193350  among other sources
 //
@@ -32,8 +26,15 @@
 //
 
 //
-//  This code is for driving a Unicorn Hat HD via Arduino, it is an experimental
-//  hack and most likely contains a large amount of redundant code from the MAX7219 system
+//  Wire the driver board to the arduino with the following pinout:
+//
+//  CS - pin 10 (this can be changed, see below)
+//  DIN - pin 11 (this is fixed by the SPI library)
+//  CLK - pin 13 (this is fixed by the SPI library)
+//
+//  My panels were arranged with the drivers cascaded BR->BL->TR->TL,
+//  however the image is drawn upside-down.
+//  If your panels are in a different order, adjust the constants below
 //
 
 #define STATUS_LIGHTS     // Drive neopixel status lights in the horns
@@ -65,7 +66,8 @@
 #define MAX_DELAY    250 // Maximum delay between blinks
 #define STATUS_DIVIDER 32  // This controls the speed of the status light chaser, bigger is slower (def 32)
 
-#define CS_PIN 10       // Chip select pin for eye panels
+#define CS_LEFT_PIN 10       // Chip select pin for left eye panel
+#define CS_RIGHT_PIN 9       // Chip select pin for right eye panel
 #define STATUS_PIN 5    // Neopixels DIN pin for status LEDs
 #define ACK_LED_PIN A0  // Flashes briefly when receiving an expression input to signal that you've got it
 #define VOICE_PIN A1    // Audio input for flashing the status pins
@@ -380,36 +382,14 @@ struct STATES states[] = {
 
 
 //
-//  Bit reversal table from https://forum.arduino.cc/index.php?topic=117966.0
-//  For flipping the sprites.  This will take 7 cycles vs 2 for each a RAM read, should be fine
-//
-
-const byte reverse[256] PROGMEM = {
-  0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0, 
-  0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8, 
-  0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4, 
-  0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC, 
-  0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2, 0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2, 
-  0x0A, 0x8A, 0x4A, 0xCA, 0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA,
-  0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6, 0x36, 0xB6, 0x76, 0xF6, 
-  0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE, 0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE,
-  0x01, 0x81, 0x41, 0xC1, 0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1,
-  0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9, 0x39, 0xB9, 0x79, 0xF9, 
-  0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5, 0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5,
-  0x0D, 0x8D, 0x4D, 0xCD, 0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD,
-  0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3, 0x33, 0xB3, 0x73, 0xF3, 
-  0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB, 0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB,
-  0x07, 0x87, 0x47, 0xC7, 0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7, 
-  0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF, 0x3F, 0xBF, 0x7F, 0xFF
-};
-
-//
 //  Here's the actual implementation
 //
 
 void setup() {
-  pinMode(CS_PIN,OUTPUT);
-  digitalWrite(CS_PIN, LOW);
+  pinMode(CS_LEFT_PIN,OUTPUT);
+  pinMode(CS_RIGHT_PIN,OUTPUT);
+  digitalWrite(CS_LEFT_PIN, LOW);
+  digitalWrite(CS_RIGHT_PIN, LOW);
   pinMode(ACK_LED_PIN,OUTPUT);
   digitalWrite(ACK_LED_PIN, LOW);
 
@@ -423,8 +403,8 @@ void setup() {
   // Set up data transfers
   SPI.begin();
   SPI.beginTransaction( SPISettings(9000000, MSBFIRST, SPI_MODE0 ) );
-  digitalWrite(CS_PIN, HIGH);
-  
+  digitalWrite(CS_LEFT_PIN, HIGH);
+  digitalWrite(CS_RIGHT_PIN, HIGH);
 
   setMessage("");
 
@@ -587,17 +567,35 @@ void drawEyeR() {
     return;
   }
 
-  sendEye(ptr);
-  return;
-  
-  for( int row=0;row<8; row++) {
-    sendData(RPANEL_TL,row+1,*ptr++);
-    sendData(RPANEL_TR,row+1,*ptr++);
+  unsigned char *out=spidata;
+  unsigned int pix=0;
+  int x,y;
+  memset(spidata,0,769);
+  *out++ = 0x72; // Header
+
+  for(y=0;y<16;y++) {
+    // Grab the entire 16 pixel row
+    pix=*ptr++;
+    pix<<=8;
+    pix|=*ptr++;
+
+    for(x=0;x<16;x++) {
+        if(pix & 0x0001) {
+            *out++ = EYE_RED;
+            *out++ = EYE_GREEN;
+            *out++ = EYE_BLUE;
+        } else {
+            *out++ = 0;
+            *out++ = 0;
+            *out++ = 0;
+        }
+        pix >>=1;
+    }
   }
-  for( int row=0;row<8; row++) {
-    sendData(RPANEL_BL,row+1,*ptr++);
-    sendData(RPANEL_BR,row+1,*ptr++);
-  }
+
+  digitalWrite(CS_RIGHT_PIN,LOW);
+  SPI.transfer(spidata,769);
+  digitalWrite(CS_RIGHT_PIN,HIGH);
 
   // Don't send again until the frame changes
   updateR=false;
@@ -605,40 +603,16 @@ void drawEyeR() {
 }
 
 //
-//  Draw the mirrored sprite on the 'left' matrix (note the L&R panels are swapped)
+//  Draw the mirrored sprite on the 'left' matrix
 //
 
 void drawEyeL() {
-#if 0  
   unsigned char *ptr = &framebuffer[0][0];
 
   if(!updateL) {
     return;
   }
 
-  sendEye();
-  return;
-  
-  for( int row=0;row<8; row++) {
-    sendData(LPANEL_TR,row+1,pgm_read_byte(&(reverse[*ptr++])));
-    sendData(LPANEL_TL,row+1,pgm_read_byte(&(reverse[*ptr++])));
-  }
-  for( int row=0;row<8; row++) {
-    sendData(LPANEL_BR,row+1,pgm_read_byte(&(reverse[*ptr++])));
-    sendData(LPANEL_BL,row+1,pgm_read_byte(&(reverse[*ptr++])));
-  }
-
-  // Don't send again until the frame changes
-  updateL=false;
-
-#endif
-}
-
-//
-//  Blit the eye to the Unicorn panel
-//
-
-void sendEye(unsigned char *ptr) {
   unsigned char *out=spidata;
   unsigned int pix=0;
   int x,y;
@@ -665,9 +639,12 @@ void sendEye(unsigned char *ptr) {
     }
   }
 
-  digitalWrite(CS_PIN,LOW);
+  digitalWrite(CS_LEFT_PIN,LOW);
   SPI.transfer(spidata,769);
-  digitalWrite(CS_PIN,HIGH);
+  digitalWrite(CS_LEFT_PIN,HIGH);
+
+  // Don't send again until the frame changes
+  updateL=false;
 }
 
 //
